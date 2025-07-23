9cec3cb78b8ca10d68481de743d92648a6884cce
EC2 Default User <ec2-user@ip-172-31-13-87.ap-southeast-2.compute.internal>
2025-07-23 15:06:16 +0000

    modify main.py

--- main.py
+++ main.py
@@ -5,12 +5,55 @@ from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
 
 from utils.prompter import Prompter
 
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+try:
+    if torch.backends.mps.is_available():
+        device = "mps"
+except:  # noqa: E722
+    pass
+
 def main():
     base_model = "yahma/llama-7b-hf"
     lora_weights = "tloen/alpaca-lora-7b"
+    load_8bit = False
     prompter = Prompter("")
-    tokenizer = LlamaTokenizer.from_pretrained(base_model,lora_weights, torch_dtype=torch.float16)
-    model = PeftModel.from_pretrained(base_model)
+    tokenizer = LlamaTokenizer.from_pretrained(base_model)
+    if device == "cuda":
+        model = LlamaForCausalLM.from_pretrained(
+            base_model,
+            load_in_8bit=load_8bit,
+            torch_dtype=torch.float16,
+            device_map="auto",
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            torch_dtype=torch.float16)
+    elif device == "mps":
+        model = LlamaForCausalLM.from_pretrained(
+            base_model,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+    else:
+        model = LlamaForCausalLM.from_pretrained(
+            base_model, device_map={"": device}, low_cpu_mem_usage=True
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
+        )
 
 
 # Press the green button in the gutter to run the script.
